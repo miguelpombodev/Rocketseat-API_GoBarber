27ae1e4f71bfe6d510102c252c0911a5547c5fd0
WallGhost <mp96dev@gmail.com>
2019-11-18 21:41:14 -0300

    Code revision and Yup installation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,12 +5,16 @@ class UserController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string().email().required(),
-      password: Yup.string().required().min(6)
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
     });
 
-    if(!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation Failed! '})
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed! ' });
     }
 
     const userExists = await User.findOne({ where: { email: req.body.email } });
@@ -36,16 +40,16 @@ class UserController {
       oldPassword: Yup.string().min(6),
       password: Yup.string()
         .min(6)
-        .when('oldPassword', (oldPassword, field) => 
-        oldPassword ? field.required() : field
-      ),
-      passwordConfirmation: Yup.string().when('password', (password, field) => 
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      passwordConfirmation: Yup.string().when('password', (password, field) =>
         password ? field.required().oneOf([Yup.ref('password')]) : field
       ),
     });
 
-    if(!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation Failed! '})
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed! ' });
     }
 
     const { email, oldPassword } = req.body;
